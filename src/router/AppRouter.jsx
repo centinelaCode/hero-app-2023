e5ad5ac4c43263408b84c7d7b8964670874efe73
centinela1024tb <centinela1024tb@gmail.com>
2023-04-12 17:53:03 -0600

    [ADD] Se crea el router principal de la aplicacion

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { DcPage } from '../heroes/pages/DcPages'
+import { LoginPage } from '../auth/pages/LoginPage'
+
+
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+         <Route path="marvel" element={<MarvelPage />} />
+         <Route path="dc" element={<DcPage />} />
+
+         <Route path="login" element={<LoginPage />} />
+
+         {/* ruta comodin */}
+         <Route path="/" element={<Navigate to='/marvel' />} />
+      </Routes>
+    </>
+  )
+}
+
