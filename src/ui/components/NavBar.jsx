d219900a359290cbc16973fb9e47dcd82b6aee7b
centinela1024tb <centinela1024tb@gmail.com>
2023-04-13 15:18:01 -0600

    [ADD] Usamos el useNavigate (custom hook de route-react-dom) para simular salir y entrar de la aplicación (login | logout)

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,7 +1,15 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
-export const NavBar = () => {   
+export const NavBar = () => {
+
+   const navigate = useNavigate();
+
+   const onLogout = () => {
+      navigate('/login', {
+         replace: true
+      })
+   }
 
    return (      
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">            
@@ -41,7 +49,10 @@ export const NavBar = () => {
                      Raul
                   </span>
 
-                  <button className="nav-item nav-link btn">
+                  <button 
+                     className="nav-item nav-link btn"
+                     onClick={ onLogout }
+                  >
                      Logout
                   </button>
 
