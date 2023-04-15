8001c805d446aac3425d6234acf55a5ee2a6093d
centinela1024tb <centinela1024tb@gmail.com>
2023-04-14 18:11:02 -0600

    [ADD] Se hace la maquetación del componente de buscar heroe
    => tendremos un input y un boton para ingresar el hero a buscar

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,12 +1,50 @@
+import { HeroCard } from '../components'
+import { useForm } from '../../hooks/useForm';
 
 export const SearchPage = () => {
+
   return (
-    <>
-      <h1 className="mt-3">Search Hero</h1>
-      <hr />
+	<>
+	<h1 className="mt-3">Search Hero</h1>
+	<hr />
 
+	<div className="row">
+		<div className="col-5">
+			<h4>Searching</h4>
+			<hr />
 
-    </>
+			<form >
+			<input 
+				type="text"
+				placeholder="Search a hero"
+				className="form-control"
+				name="searchText"
+				autoComplete="off"
+				
+			/>
+
+			<button className="btn btn-outline-primary mt-2">Search</button>
+			</form>
+		</div>
+
+		<div className="col-7">
+			<h4>Results</h4>
+			<hr />
+
+			<div 
+				className="alert alert-primary"
+			>Search A Hero</div>
+
+			<div 
+				className="alert alert-danger"
+			>No found hero with <b>ABC</b></div>
+
+			{/* <HeroCard heroe={...heroe} ></HeroCard> */}
+
+		</div>
+	</div>
+
+	</>
   )
 }
  
