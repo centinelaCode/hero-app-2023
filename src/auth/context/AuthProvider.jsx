7eb56dbb2fe445281c41242bf3a2ecfabf76595b
centinela1024tb <centinela1024tb@gmail.com>
2023-04-17 17:42:28 -0600

    [ADD] guardamos el user en localstorage usando la función init del useReducer

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,24 +4,35 @@ import { AuthContext } from './AuthContext'
 import { authReducer } from './authReducer'
 import { types } from '../types/types'
 
-const initialState = {
-   logged: false,
+// const initialState = {
+//     logged: false,
+// }
+
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') )
+
+  return {
+    logged: !!user,
+    user,
+  }
 }
 
 export const AuthProvider = ({ children }) => {
 
-  const [ authState, dispatch ] = useReducer(authReducer, initialState);
+  const [ authState, dispatch ] = useReducer(authReducer, {}, init);
 
-
-    // definimos la accion para establecer el login
+  // definimos la accion para establecer el login
   const login = ( name = '' ) => {
+
+    const user = { id: 'ABC', name }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name
-      }
+      payload: user
     }
+
+    // guardamos al user en el localstorage
+    localStorage.setItem('user', JSON.stringify( user ));
     
     // dispara una accion que se definio previamente
     dispatch(action);
